1c17083fc6d4c72a6fb52760aaa77a6fc0476bcb
Andrei Dutkovsky <andreidutkovsly@yandex.ru>
2024-02-26 13:11:44 +0300

    styles custom modal

--- src/finishPlugin/customModal.js
+++ src/finishPlugin/customModal.js
@@ -46,103 +46,6 @@ const ra_All = {
   ra3_6,
 };
 
-function createFormForModal(parent, formData) {
-  const formContainer = document.createElement("div");
-  formContainer.id = "formContainer";
-  parent.appendChild(formContainer);
-
-  const form = document.createElement("form");
-  form.className = "form-edit-svg";
-  for (const key in formData) {
-    const label = document.createElement("label");
-    label.textContent = key + ":";
-    const input = document.createElement("input");
-    input.type = "text";
-    input.name = key;
-    input.value = formData[key];
-    label.appendChild(input);
-    form.appendChild(label);
-  }
-
-  form.onsubmit = function (e) {
-    const formValues = {};
-    e.preventDefault();
-    const target = e.target;
-    const formData = new FormData(target);
-    for (const [name, value] of formData.entries()) {
-      formValues[name] = value;
-    }
-    emitter.emit("insertIcon", formValues);
-    closeEditSvgModal();
-  };
-
-  const submitButton = document.createElement("button");
-  submitButton.type = "submit";
-  submitButton.textContent = "Вставить";
-
-  form.appendChild(submitButton);
-  formContainer.appendChild(form);
-}
-
-export function openEditSvgModal(iconData) {
-  const myModal = document.getElementById("myModal");
-  if (myModal) myModal.style.display = "block";
-
-  const formData = setIconHeadForm(iconData.icon);
-  createFormForModal(document.getElementById("wrapForm"), formData);
-  toggleCloseEvent(true);
-}
-
-function setIconHeadForm(svgString) {
-  const parser = new DOMParser();
-  const svgDocument = parser.parseFromString(svgString, "image/svg+xml");
-  const svgElement = svgDocument.documentElement;
-  const textElements = svgDocument.querySelectorAll("text");
-  const textValues = {};
-
-  textElements?.forEach((textElement) => {
-    const content = textElement.textContent || "";
-    // textElement.id = 'text_' + content
-    if (content.length <= 6) textValues[content] = "";
-  });
-  const headFormSvg = document.getElementById("headFormSvg");
-  if (headFormSvg) headFormSvg.appendChild(svgElement);
-  return textValues;
-}
-
-export function closeEditSvgModal() {
-  const myModal = document.getElementById("myModal");
-  if (myModal) myModal.style.display = "none";
-
-  const formContainer = document.getElementById("formContainer");
-  const svg = document.querySelector("#headFormSvg svg");
-  formContainer?.remove();
-  svg?.remove();
-
-  toggleCloseEvent(false);
-}
-
-function toggleCloseEvent(isOpen) {
-  const closeBtn = document.getElementById("closeModal");
-
-  if (closeBtn)
-    if (isOpen) {
-      closeBtn.onclick = closeEditSvgModal;
-    } else {
-      closeBtn.onclick = null;
-    }
-}
-
-// Закрыть модальное окно при клике вне его области
-window.onclick = function (event) {
-  const modal = document.getElementById("myModal");
-  if (event.target === modal) {
-    closeEditSvgModal();
-  }
-};
-
-export const testFormData = { key1: "", key2: "", key3: "" };
-
 function addModal(content) {
   var modal = document.createElement("div");
   var closeButton = document.createElement("button");
@@ -151,8 +54,10 @@ function addModal(content) {
   closeButton.innerHTML = "&times;";
   modalContent.innerHTML = content;
 
+  modalContent.className = 'modal-roughness-wrap-content';
+
   // Стили модального окна
-  modal.id = "modalTest";
+  modal.id = "modalRoughness";
   modal.style.position = "fixed";
   modal.style.top = "0";
   modal.style.left = "0";
